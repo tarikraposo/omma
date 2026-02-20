87cf6f54dca93b88e20a389ff89cd036382449ff
Tarik Raposo <tarik.parreira@mpdft.mp.br>
2026-02-20 16:15:20 -0300

    [#3] Finaliza e importa componente de newsletter nas seções da homepge.

--- src/app/components/recipe-newsletter/recipe-newsletter.ts
+++ src/app/components/recipe-newsletter/recipe-newsletter.ts
@@ -1,11 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-recipe-newsletter',
-  imports: [],
+  standalone: true,
+  imports: [
+    FormsModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+  ],
   templateUrl: './recipe-newsletter.html',
   styleUrl: './recipe-newsletter.scss',
 })
 export class RecipeNewsletter {
+  email = signal('');
+  submitted = signal(false);
 
+  submit() {
+    if (!this.email()) return;
+
+    this.submitted.set(true);
+
+    // aqui você poderia chamar API futuramente
+    console.log('Email cadastrado:', this.email());
+  }
 }
--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 import { RecipeCategories } from "../../components/recipe-categories/recipe-categories";
+import { RecipeHero } from "../../components/recipe-hero/recipe-hero";
+import { RecipeGrid } from "../../components/recipe-grid/recipe-grid";
+import { RecipeFeatured } from "../../components/recipe-featured/recipe-featured";
+import { RecipeNewsletter } from "../../components/recipe-newsletter/recipe-newsletter";
 
 @Component({
   selector: 'app-home',
-  imports: [RecipeCategories],
+  imports: [RecipeCategories, RecipeHero, RecipeGrid, RecipeFeatured, RecipeNewsletter],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
