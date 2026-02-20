f8ec739e8eecca30eb1e2d41e9d93cf469a8611b
Tarik Raposo <tarik.parreira@mpdft.mp.br>
2026-02-20 16:02:12 -0300

    [#3] Cria componente de grid para as seções da homepge.

---
+++ src/app/components/recipe-grid/recipe-grid.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recipe-grid',
+  imports: [],
+  templateUrl: './recipe-grid.html',
+  styleUrl: './recipe-grid.scss',
+})
+export class RecipeGrid {
+
+}
