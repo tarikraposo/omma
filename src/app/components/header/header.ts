b67274f8b1d6113231e51cdfdcde6e24a5db3e98
Tarik Raposo <tarik.parreira@mpdft.mp.br>
2026-02-20 15:29:11 -0300

    [#2] Finaliza componente de header inicial.

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,11 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { NgIf } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  standalone: true,
+  imports: [
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSidenavModule,
+    FormsModule
+  ],
   templateUrl: './header.html',
-  styleUrl: './header.scss',
+  styleUrl: './header.scss'
 })
 export class Header {
 
+  isSearchOpen = signal(false);
+  isMenuOpen = signal(false);
+
+  toggleSearch() {
+    this.isSearchOpen.update(v => !v);
+  }
+
+  toggleMenu() {
+    this.isMenuOpen.update(v => !v);
+  }
 }
