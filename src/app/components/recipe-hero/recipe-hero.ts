a02f8b9e82c4a20c4ac83ed405b8327af2bd0809
Tarik Raposo <tarik.parreira@mpdft.mp.br>
2026-02-20 16:08:19 -0300

    [#3] Cria componente para hero section.

---
+++ src/app/components/recipe-hero/recipe-hero.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recipe-hero',
+  imports: [],
+  templateUrl: './recipe-hero.html',
+  styleUrl: './recipe-hero.scss',
+})
+export class RecipeHero {
+
+}
