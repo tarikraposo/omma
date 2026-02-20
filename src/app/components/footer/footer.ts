3eb558b5e8cdabd358bee737ed71ab7184cc60b8
Tarik Raposo <tarik.parreira@mpdft.mp.br>
2026-02-20 15:50:10 -0300

    [#3] Adiciona footer, não adicionado na última issue.

--- src/app/components/footer/footer.ts
+++ src/app/components/footer/footer.ts
@@ -1,11 +1,36 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  standalone: true,
+  imports: [
+    MatButtonModule,
+    MatDividerModule,
+  ],
   templateUrl: './footer.html',
-  styleUrl: './footer.scss',
+  styleUrl: './footer.scss'
 })
 export class Footer {
+
+  receitas = [
+    'Pratos Quentes',
+    'Sobremesas',
+    'Confeitaria',
+    'Saladas'
+  ];
+
+  sobre = [
+    'Nossa História',
+    'Equipe',
+    'Parcerias'
+  ];
+
+  redes = [
+    'Instagram',
+    'YouTube',
+    'Pinterest'
+  ];
 
 }
