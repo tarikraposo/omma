8c0db3333bace3738006322e48967dd351ac6d4a
Tarik Raposo <tarik.parreira@mpdft.mp.br>
2026-02-20 15:51:18 -0300

    [#3] Cria componente para as categorias.

---
+++ src/app/components/recipe-categories/recipe-categories.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recipe-categories',
+  imports: [],
+  templateUrl: './recipe-categories.html',
+  styleUrl: './recipe-categories.scss',
+})
+export class RecipeCategories {
+
+}
